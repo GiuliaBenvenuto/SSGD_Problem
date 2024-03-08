abada9164f1a0c2ea61b78d91e0e356f5f7ab9e0
GiuliaBenvenuto <giulia.benvenuto@outlook.com>
2024-03-08 09:44:23 +0100

    main.cpp wireframe and shading sections

--- main.cpp
+++ main.cpp
@@ -11,56 +11,65 @@
 using namespace std;
 using namespace cinolib;
 
-struct State {
-  // Program state
-  bool MESH_IS_LOADED;
-  bool SHOW_MESH;
-  bool SHOW_WIREFRAME;
+//:::::::::::::::::::::::::::: GLOBAL VARIABLES (FOR GUI) ::::::::::::::::::::::::::::::
 
+struct State {
+  // Program state ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  bool MESH_IS_LOADED;
   // Input
-  DrawableTrimesh<> m;  // the input mesh
-  uint nverts;          // its #vertices
-  vector<vector<uint>> VV;  // its VV relation
+  DrawableTrimesh<> m;            // the input mesh
+  uint nverts;                    // its #vertices
+  vector<vector<uint>> VV;        // its VV relation
+  
+  // GUI state ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
   // View
-  // bool show_m, show_wf;  // show mesh, wire-frame
+  bool SHOW_MESH, SHOW_WIREFRAME;
   // Wireframe
   float wireframe_width;  // Wireframe width
   float wireframe_alpha;  // Wireframe transparency
+  // Shading
+  enum MeshRenderMode { RENDER_POINTS, RENDER_FLAT, RENDER_SMOOTH } render_mode;
+
+
 
   State() {
     MESH_IS_LOADED = false;
-    SHOW_MESH = true;
+    // view
     SHOW_WIREFRAME = false;
+    SHOW_MESH = true;
 
     wireframe_width = 1.0;
     wireframe_alpha = 1.0;
   }
 };
 
+//::::::::::::::::::::::::::::::::::::GUI utilities ::::::::::::::::::::::::::::::::::::
+
+
+
 
 //::::::::::::::::::::::::::::::::::::I/O ::::::::::::::::::::::::::::::::::::
 
-void Load_mesh(string filename, GLcanvas & gui, State &gs) {
-    gs.m = DrawableTrimesh<>(filename.c_str());
-    gs.nverts = gs.m.num_verts();
-    gs.VV.resize(gs.nverts); // fill in Vertex-Vertex relation
-    for (auto i = 0; i < gs.nverts; i++) gs.VV[i] = gs.m.vert_ordered_verts_link(i);
-
-    // Uncomment the following and adjust parameters if you want a smoother mesh
-    // MCF(gs.m, 12, 1e-5, true);
-
-    gs.m.normalize_bbox(); // Rescale mesh to fit [0,1]^3 box
-    gs.m.center_bbox();
-    gs.m.show_wireframe(gs.SHOW_WIREFRAME);
-    gs.m.show_mesh(gs.SHOW_MESH);
-    gs.m.updateGL();
-
-    if (!gs.MESH_IS_LOADED) {
-        gui.push(&gs.m);
-        gs.MESH_IS_LOADED = true;
-    }
+void Load_mesh(string filename, GLcanvas & gui, State &gs)
+{
+  gs.m = DrawableTrimesh<>(filename.c_str());
+  gs.nverts = gs.m.num_verts();
+  gs.VV.resize(gs.nverts); // fill in Vertex-Vertex relation
+  for (auto i=0;i<gs.nverts;i++) gs.VV[i]=gs.m.vert_ordered_verts_link(i);
+  // uncomment the following and adjust parameters if you want a smoother mesh
+  // MCF(m,12,1e-5,true);
+  gs.m.normalize_bbox(); // rescale mesh to fit [0,1]^3 box
+  gs.m.center_bbox();
+  gs.m.show_wireframe(gs.SHOW_WIREFRAME);
+  gs.m.show_mesh(gs.SHOW_MESH);    
+  gs.m.updateGL();  
+  // gs.point_size = gs.m.edge_avg_length()/2; // set initial radius of spheres for critical points
+  if (!gs.MESH_IS_LOADED) {
+    gui.push(&gs.m);
+    gs.MESH_IS_LOADED = true;
+  }
 }
-
 
 void Load_mesh(GLcanvas & gui, State &gs)
 {
@@ -69,7 +78,24 @@ void Load_mesh(GLcanvas & gui, State &gs)
 }
 
 
+
+
+
+//====================== GENERAL UTILITIES =============================================
+
+
+
+//==================== FIELD GENERATORS ========================
+
+
+//=============================== INPUT FIELD ==================================
+
+
+//=========================== SCALE-SPACE FUNCTIONS =============================
+
+
 //::::::::::::::::::::::::::::::::: GUI ::::::::::::::::::::::::::::::::::::::::::::::::
+
 GLcanvas Init_GUI()
 {
   GLcanvas gui(1500,700);
@@ -78,59 +104,75 @@ GLcanvas Init_GUI()
   return gui;
 }
 
-void Setup_GUI_Callbacks(GLcanvas & gui, State &gs) {
+void Setup_GUI_Callbacks(GLcanvas & gui, State &gs)
+{
   gui.callback_app_controls = [&]() {
-    ImGui::SeparatorText("Mesh Operations");
-
-    if (ImGui::Button("Load mesh")) {
-      if (gs.MESH_IS_LOADED) {
-        ImGui::OpenPopup("Load mesh?");
-      } else {
-        Load_mesh(gui, gs);
+    // Files
+    ImGui::SeparatorText("Files");
+    ImGui::SetNextItemOpen(true, ImGuiCond_Once);
+    if (ImGui::TreeNode("IO")) {
+      if (ImGui::Button("Load mesh")) {
+        if (gs.MESH_IS_LOADED) {
+          ImGui::OpenPopup("Load mesh?");
+        } else {
+          Load_mesh(gui,gs);
+        }
       }
-    }
-
-    if (ImGui::BeginPopupModal("Load mesh?", NULL, ImGuiWindowFlags_AlwaysAutoResize)) {
-      ImGui::Text("This will replace the current mesh.");
-      if (ImGui::Button("OK")) {
-        Load_mesh(gui, gs);
-        ImGui::CloseCurrentPopup();
+      // Modal popup for loading files
+      ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+      ImGui::SetNextWindowPos(center,ImGuiCond_Appearing,ImVec2(0.5f,0.5f));
+      if (ImGui::BeginPopupModal("Load mesh?", NULL, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse))
+      {
+        static bool dont_ask_me_next_time = false;
+        if (dont_ask_me_next_time) {Load_mesh(gui,gs); ImGui::CloseCurrentPopup();}
+        ImGui::Text("All data structures will be reset - Load anyway?\n\n");
+        ImGui::Separator();           
+        ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0,0));
+        ImGui::Checkbox("Don't ask me next time", &dont_ask_me_next_time);
+        ImGui::PopStyleVar();
+        if (ImGui::Button("OK", ImVec2(120,0))) {Load_mesh(gui,gs); ImGui::CloseCurrentPopup();}
+        ImGui::SameLine();
+        if (ImGui::Button("Cancel", ImVec2(120,0))) {ImGui::CloseCurrentPopup();}
+        ImGui::EndPopup();
       }
-      ImGui::SameLine();
-      if (ImGui::Button("Cancel")) {
-        ImGui::CloseCurrentPopup();
-      }
-      ImGui::EndPopup();
+      ImGui::TreePop();
     }
-
-    ImGui::Checkbox("Show Mesh", &gs.SHOW_MESH);
-    if (gs.SHOW_MESH) {
-      gs.m.show_mesh(true);
-    } else {
-      gs.m.show_mesh(false);
-    }
-
-    ImGui::Checkbox("Show Wireframe", &gs.SHOW_WIREFRAME);
-    if (gs.SHOW_WIREFRAME) {
-      gs.m.show_wireframe(true);
-    } else {
-      gs.m.show_wireframe(false);
-    }
+    
 
     // Wireframe settings
     ImGui::SetNextItemOpen(true, ImGuiCond_Once);
     if (ImGui::TreeNode("Wireframe")) {
       if (ImGui::Checkbox("Show", &gs.SHOW_WIREFRAME)) gs.m.show_wireframe(gs.SHOW_WIREFRAME);
       if (ImGui::SliderFloat("Width", &gs.wireframe_width, 1.f, 10.f)) gs.m.show_wireframe_width(gs.wireframe_width);
-      // Note: If DrawableTrimesh does not support wireframe transparency directly, this will not work.
-      // This line is only useful if you have a method to set wireframe transparency in DrawableTrimesh.
-      if (ImGui::SliderFloat("Transparency", &gs.wireframe_alpha, 0.f, 1.f)) {} // Placeholder for transparency control
+      if (ImGui::SliderFloat("Transparency", &gs.wireframe_alpha, 0.f, 1.f)) gs.m.show_wireframe_transparency(gs.wireframe_alpha);
+      ImGui::TreePop();
+    }
+
+    // Mesh shading
+    ImGui::SetNextItemOpen(true, ImGuiCond_Once);
+    if (ImGui::TreeNode("Shading")) {
+      // Point rendering mode
+      if(ImGui::RadioButton("Point ", gs.render_mode == State::RENDER_POINTS)) {
+        gs.render_mode = State::RENDER_POINTS;
+        gs.m.show_mesh_points();
+      }
+      // Flat shading mode
+      if(ImGui::RadioButton("Flat  ", gs.render_mode == State::RENDER_FLAT)) {
+        gs.render_mode = State::RENDER_FLAT;
+        gs.m.show_mesh_flat(); 
+      }
+      // Smooth shading mode
+      if(ImGui::RadioButton("Smooth", gs.render_mode == State::RENDER_SMOOTH)) {
+        gs.render_mode = State::RENDER_SMOOTH;
+        gs.m.show_mesh_smooth(); // Assuming you have a method to display smooth shaded mesh
+      }
+
 
       ImGui::TreePop();
     }
 
-    if (gs.MESH_IS_LOADED) gs.m.updateGL();
-  };
+
+   };
 }
 
 
@@ -142,12 +184,12 @@ int main(int argc, char **argv) {
   //SETUP GLOBAL STATE AND GUI:::::::::::::::::::::::
   State gs;
   GLcanvas gui = Init_GUI();
-  Setup_GUI_Callbacks(gui,gs);
+  Setup_GUI_Callbacks(gui, gs);
 
   //Load mesh
   if (argc>1) {
     string s = "../data/" + string(argv[1]);
-    Load_mesh(s,gui,gs);
+    Load_mesh(s, gui, gs);
   }
 
   // // // GENERATE FIELD:::::::::::::::::::::::::::::::
