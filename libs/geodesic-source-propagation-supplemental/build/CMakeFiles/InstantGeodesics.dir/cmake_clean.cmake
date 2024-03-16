5b7f2175fbde7b006bda45fdb7ae7eea78bf0c88
ClaudioMancinelli <claudio.f.mancinelli@gmail.com>
2024-03-16 09:29:44 +0100

    trettner method

---
+++ libs/geodesic-source-propagation-supplemental/build/CMakeFiles/InstantGeodesics.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/InstantGeodesics.dir/main.cc.o"
+  "CMakeFiles/InstantGeodesics.dir/main.cc.o.d"
+  "InstantGeodesics"
+  "InstantGeodesics.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/InstantGeodesics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
