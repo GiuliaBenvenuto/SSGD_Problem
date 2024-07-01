dcfa4da0e5034a909812fd337b9be5c373f98183
GiuliaBenvenuto <giulia.benvenuto@outlook.com>
2024-07-01 11:09:16 +0200

    iterations for each num_subdivision = 1, no vertex normals and colors

--- pymeshlab/Esperimento_1/prova.py
+++ pymeshlab/Esperimento_1/prova.py
@@ -2,7 +2,8 @@
 import pymeshlab as ml
 import numpy as np
 
-def generate_refined_meshes(mesh_base_name, num_subdivisions=3):
+def generate_refined_meshes(mesh_base_name, num_subdivisions):
+    print(f"----- Generating refined meshes for {mesh_base_name} with {num_subdivisions} subdivisions -----")
     mesh_name = f'{mesh_base_name}.obj'
 
     # Function to parse the .obj file
@@ -31,22 +32,26 @@ def generate_refined_meshes(mesh_base_name, num_subdivisions=3):
     ms.add_mesh(mesh, mesh_name)
 
     # print 20 vertices coords of the mesh
-    for i in range(20):
-        print(f"Vertex {i}: {ms.current_mesh().vertex_matrix()[i]}")
+    # for i in range(20):
+    #     print(f"Vertex {i}: {ms.current_mesh().vertex_matrix()[i]}")
     
     # print 20 faces of the mesh
-    for i in range(20):
-        print(f"Face {i}: {ms.current_mesh().face_matrix()[i]}")
+    # for i in range(20):
+    #     print(f"Face {i}: {ms.current_mesh().face_matrix()[i]}")
 
 
     # Apply the subdivision filter multiple times
     for i in range(num_subdivisions):
-        ms.apply_filter('meshing_surface_subdivision_loop', loopweight='Loop', iterations=3, threshold=ml.PercentageValue(0), selected=False)
+        ms.apply_filter('meshing_surface_subdivision_loop', loopweight='Loop', iterations=1, threshold=ml.PercentageValue(0), selected=False)
         current_vertices = ms.current_mesh().vertex_matrix()
         print(f"After subdivision {i+1}: {current_vertices.shape[0]} vertices, sample vertex: {current_vertices[0]}")
 
         save_path = f'{mesh_base_name}_subdiv_{i+1}.obj'
-        ms.save_current_mesh(save_path)
+        
+        # Save without vertex normals and colors:
+        # ms.save_current_mesh(save_path)
+        ms.save_current_mesh(save_path, save_vertex_normal=False, save_vertex_color=False)
+    
         print(f"Mesh saved to {save_path}")
 
         # Clear and reload the mesh
@@ -58,8 +63,8 @@ def generate_refined_meshes(mesh_base_name, num_subdivisions=3):
 
 
 # Use the function to generate refined meshes
-# generate_refined_meshes('data/altro/bunny_1k', 3)
-generate_refined_meshes('data/bob/bob_tri', 3)
+generate_refined_meshes('data/altro/bunny_1k', 3)
+# generate_refined_meshes('data/bob/bob_tri', 3)
 # generate_refined_meshes('data/altro/Nefertiti', 3)
 
 
